86d179a9e303dedcbafe9c9fadef610f151ecb37
museun <museun@outlook.com>
2019-03-13 09:14:30 -0400

    unescape tags in system_msg() @ usernotice

--- src/twitch/commands/usernotice.rs
+++ src/twitch/commands/usernotice.rs
@@ -147,8 +147,16 @@ impl UserNotice {
         self.get_parsed("room-id").unwrap()
     }
     /// The message printed in chat along with this notice.
-    pub fn system_msg(&self) -> &str {
-        self.get("system-msg").unwrap()
+    pub fn system_msg(&self) -> String {
+        self.get("system-msg")
+            .map(|s| {
+                s.replace("\\s", " ")
+                    .replace("\\r", "\r")
+                    .replace("\\n", "\n")
+                    .replace("\\\\", "\\")
+                    .replace("\\:", ":")
+            })
+            .unwrap()
     }
     /// Timestamp of when the notice was sent
     pub fn tmi_sent_ts(&self) -> u64 {
@@ -228,4 +236,34 @@ pub enum NoticeType {
     BitsBadgeTier,
     /// An unknown message type
     Unknown(String),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn system_msg_unescape() {
+        let input = [
+r#"@badges=staff/1,broadcaster/1,turbo/1;color=#008000;display-name=ronni;emotes=;id=db25007f-7a18-43eb-9379-80131e44d633;login=ronni;mod=0;msg-id=resub;msg-param-cumulative-months=6;msg-param-streak-months=2;msg-param-should-share-streak=1;msg-param-sub-plan=Prime;msg-param-sub-plan-name=Prime;room-id=1337;subscriber=1;system-msg=ronni\shas\ssubscribed\sfor\s6\smonths!;tmi-sent-ts=1507246572675;turbo=1;user-id=1337;user-type=staff :tmi.twitch.tv USERNOTICE #dallas :Great stream -- keep it up!"#,
+r#"@badges=broadcaster/1,subscriber/6;color=;display-name=qa_subs_partner;emotes=;flags=;id=b1818e3c-0005-490f-ad0a-804957ddd760;login=qa_subs_partner;mod=0;msg-id=anonsubgift;msg-param-months=3;msg-param-recipient-display-name=TenureCalculator;msg-param-recipient-id=135054130;msg-param-recipient-user-name=tenurecalculator;msg-param-sub-plan-name=t111;msg-param-sub-plan=1000;room-id=196450059;subscriber=1;system-msg=An\sanonymous\suser\sgifted\sa\sTier\s1\ssub\sto\sTenureCalculator!\s;tmi-sent-ts=1542063432068;turbo=0;user-id=196450059;user-type= :tmi.twitch.tv USERNOTICE #qa_subs_partner"#,
+r#"@badges=turbo/1;color=#9ACD32;display-name=TestChannel;emotes=;id=3d830f12-795c-447d-af3c-ea05e40fbddb;login=testchannel;mod=0;msg-id=raid;msg-param-displayName=TestChannel;msg-param-login=testchannel;msg-param-viewerCount=15;room-id=56379257;subscriber=0;system-msg=15\sraiders\sfrom\sTestChannel\shave\sjoined\n!;tmi-sent-ts=1507246572675;tmi-sent-ts=1507246572675;turbo=1;user-id=123456;user-type= :tmi.twitch.tv USERNOTICE #othertestchannel"#,
+r#"@badges=;color=;display-name=SevenTest1;emotes=30259:0-6;id=37feed0f-b9c7-4c3a-b475-21c6c6d21c3d;login=seventest1;mod=0;msg-id=ritual;msg-param-ritual-name=new_chatter;room-id=6316121;subscriber=0;system-msg=Seventoes\sis\snew\shere!;tmi-sent-ts=1508363903826;turbo=0;user-id=131260580;user-type= :tmi.twitch.tv USERNOTICE #seventoes :HeyGuys"#,
+        ];
+
+        let output = [
+            "ronni has subscribed for 6 months!",
+            "An anonymous user gifted a Tier 1 sub to TenureCalculator! ",
+            "15 raiders from TestChannel have joined\n!",
+            "Seventoes is new here!",
+        ];
+
+        for (input, &output) in input.iter().zip(output.iter()) {
+            let msg = crate::irc::types::Message::parse(input).unwrap();
+            if let crate::twitch::Message::UserNotice(msg @ UserNotice { .. }) =
+                super::parse(&msg).unwrap()
+            {
+                assert_eq!(msg.system_msg(), output);
+            }
+        }
+    }
 }
