c5264422c3298c428c6ad2a8935e62e93e92212d
museun <museun@outlook.com>
2020-02-06 16:53:59 -0500

    sealed traits should be feature gated too

--- src/internal.rs
+++ src/internal.rs
@@ -20,18 +20,20 @@ pub(crate) mod private {
         impl<T> Sealed for T where T: crate::AsOwned {}
     }
 
-    pub(crate) mod event_marker {
-        pub trait Sealed {}
-        impl<'a, T> Sealed for T where T: crate::client::Event<'a> {}
-    }
+    cfg_async! {
+        pub(crate) mod event_marker {
+            pub trait Sealed {}
+            impl<'a, T> Sealed for T where T: crate::client::Event<'a> {}
+        }
 
-    pub(crate) mod mapped_marker {
-        pub trait Sealed<E> {}
-        impl<'a, T, E> Sealed<E> for T
-        where
-            T: crate::client::EventMapped<'a, E>,
-            E: crate::client::Event<'a>,
-        {
+        pub(crate) mod mapped_marker {
+            pub trait Sealed<E> {}
+            impl<'a, T, E> Sealed<E> for T
+            where
+                T: crate::client::EventMapped<'a, E>,
+                E: crate::client::Event<'a>,
+            {
+            }
         }
     }
 }
