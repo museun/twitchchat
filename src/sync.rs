ea8a61e396ea2b320cd5f78bc5213d50f6c3ec42
museun <museun@outlook.com>
2020-03-03 16:31:08 -0500

    touch up sync module

--- src/sync.rs
+++ src/sync.rs
@@ -1,5 +1,7 @@
-use crate::*;
-use std::io::{Read, Write};
+use crate::{simple_user_config, UserConfig, TWITCH_IRC_ADDRESS};
+
+use std::io::Write;
+use std::net::TcpStream;
 
 /**
 Write the provided `UserConfig` to the ***sync*** writer
@@ -7,18 +9,20 @@ Write the provided `UserConfig` to the ***sync*** writer
 ```rust
 # use twitchchat::{UserConfig, sync::*};
 let config = UserConfig::builder().anonymous().build().unwrap();
+
 let mut writer = vec![];
 register(&config, &mut writer).unwrap();
+
 assert_eq!(
     std::str::from_utf8(&writer).unwrap(),
     "PASS justinfan1234\r\nNICK justinfan1234\r\n"
 );
 ```
 */
-pub fn register<W: ?Sized>(user_config: &UserConfig, writer: &mut W) -> std::io::Result<()>
-where
-    W: Write,
-{
+pub fn register<W: ?Sized + Write>(
+    user_config: &UserConfig,
+    writer: &mut W,
+) -> std::io::Result<()> {
     let UserConfig {
         name,
         token,
@@ -30,22 +34,21 @@ where
         writer.write_all(b"\r\n")?;
     }
 
-    writer.write_all(format!("PASS {}\r\n", token).as_bytes())?;
-    writer.write_all(format!("NICK {}\r\n", name).as_bytes())?;
-
-    Ok(())
+    let data = format!("PASS {}\r\nNICK {}\r\n", token, name);
+    writer.write_all(data.as_bytes())?;
+    writer.flush()
 }
 
 /**
 Opens a ***sync*** TCP connection using the provided `UserConfig`
 
 # Note
-This doesn't support TLS because TlsStream isn't clonable.
+This doesn't support TLS because ***TlsStream*** isn't clonable.
 
 Use the [async version][async] if you want a TLS wrapped connection
 
 # Example
-```rust
+```rust,no_run
 # use twitchchat::{sync::*, UserConfig};
 let user_config = UserConfig::builder().anonymous().build().unwrap();
 let (read, write) = connect(&user_config).unwrap();
@@ -53,8 +56,8 @@ let (read, write) = connect(&user_config).unwrap();
 
 [async]: ../fn.connect.html
 */
-pub fn connect(user_config: &UserConfig) -> std::io::Result<(impl Read, impl Write)> {
-    let mut stream = std::net::TcpStream::connect(TWITCH_IRC_ADDRESS)?;
+pub fn connect(user_config: &UserConfig) -> std::io::Result<(TcpStream, TcpStream)> {
+    let mut stream = TcpStream::connect(TWITCH_IRC_ADDRESS)?;
     register(user_config, &mut stream)?;
     Ok((stream.try_clone().unwrap(), stream))
 }
@@ -70,7 +73,7 @@ This doesn't support TLS because TlsStream isn't clonable.
 Use the [async version][async] if you want a TLS wrapped connection
 
 # Example
-```rust
+```rust,no_run
 # use twitchchat::{sync::*, ANONYMOUS_LOGIN};
 let (nick, pass) = ANONYMOUS_LOGIN;
 let (read, write) = connect_easy(&nick, &pass).unwrap();
@@ -79,8 +82,11 @@ let (read, write) = connect_easy(&nick, &pass).unwrap();
 [Capabilities]: ../enum.Capability.html
 [async]: ../fn.connect_easy.html
 */
-
-pub fn connect_easy(name: &str, token: &str) -> std::io::Result<(impl Read, impl Write)> {
+pub fn connect_easy(name: &str, token: &str) -> std::io::Result<(TcpStream, TcpStream)> {
     let config = simple_user_config(name, token).unwrap();
     connect(&config)
 }
+
+// TODO: should the encoder be in here?
+// TODO: should this module even exists?
+// TODO: add a SyncClient if we want to keep this
