d70ee05d8b64a7d4ae1edc6dff3cb8c37767a3e1
museun <museun@outlook.com>
2020-07-30 17:37:54 -0400

    add RoomState

--- src/ng/messages/room_state.rs
+++ src/ng/messages/room_state.rs
@@ -1,6 +1,53 @@
-use super::{AsOwned, FromIrcMessage, InvalidMessage, IrcMessage, Reborrow, Str, Validator};
+use crate::ng::{FromIrcMessage, InvalidMessage, Validator};
+use crate::ng::{IrcMessage, Str, StrIndex, TagIndices, Tags};
 
-#[derive(Debug, Clone)]
-pub struct RoomState<'a> {
-    s: &'a (),
+#[derive(Debug, Clone, PartialEq)]
+pub struct RoomState<'t> {
+    raw: Str<'t>,
+    tags: TagIndices,
+    channel: StrIndex,
+}
+
+impl<'a> FromIrcMessage<'a> for RoomState<'a> {
+    type Error = InvalidMessage;
+    fn from_irc(msg: IrcMessage<'a>) -> Result<Self, Self::Error> {
+        msg.expect_command(IrcMessage::ROOMSTATE)?;
+
+        let this = Self {
+            tags: msg.parse_tags(),
+            channel: msg.expect_arg_index(0)?,
+            raw: msg.raw,
+        };
+
+        Ok(this)
+    }
+}
+
+impl<'t> RoomState<'t> {
+    raw!();
+    tags!();
+    str_field!(channel);
+}
+
+serde_struct!(RoomState { raw, tags, channel });
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ng::irc;
+
+    #[test]
+    fn user_state_serde() {
+        let input = ":tmi.twitch.tv ROOMSTATE #museun\r\n";
+        crate::ng::serde::round_trip_json::<RoomState>(input);
+    }
+
+    #[test]
+    fn user_state() {
+        let input = ":tmi.twitch.tv ROOMSTATE #museun\r\n";
+        for msg in irc::parse(input).map(|s| s.unwrap()) {
+            let msg = RoomState::from_irc(msg).unwrap();
+            assert_eq!(msg.channel(), "#museun");
+        }
+    }
 }
