0eb8a9d7c36ddb8c811ca33268771925ee46e49c
museun <museun@outlook.com>
2020-07-30 19:45:47 -0400

    enable AllCommands

--- src/ng/messages/all_commands.rs
+++ src/ng/messages/all_commands.rs
@@ -1,54 +1,74 @@
 use super::*;
 use crate::ng::{FromIrcMessage, InvalidMessage};
 
+#[derive(Debug, Clone, PartialEq)]
 pub enum AllCommands<'a> {
     Raw(IrcMessage<'a>),
     IrcReady(IrcReady<'a>),
     Ready(Ready<'a>),
     Cap(Cap<'a>),
     ClearChat(ClearChat<'a>),
-    // ClearMsg(ClearMsg<'a>),
-    // GlobalUserState(GlobalUserState<'a>),
-    // HostTarget(HostTarget<'a>),
-    // Join(Join<'a>),
-    // Notice(Notice<'a>),
-    // Part(Part<'a>),
-    // Ping(Ping<'a>),
-    // Pong(Pong<'a>),
-    // Privmsg(Privmsg<'a>),
-    // Reconnect(Reconnect<'a>),
-    // RoomState(RoomState<'a>),
-    // UserNotice(UserNotice<'a>),
-    // UserState(UserState<'a>),
-    // Whisper(Whisper<'a>),
+    ClearMsg(ClearMsg<'a>),
+    GlobalUserState(GlobalUserState<'a>),
+    HostTarget(HostTarget<'a>),
+    Join(Join<'a>),
+    Notice(Notice<'a>),
+    Part(Part<'a>),
+    Ping(Ping<'a>),
+    Pong(Pong<'a>),
+    Privmsg(Privmsg<'a>),
+    Reconnect(Reconnect<'a>),
+    RoomState(RoomState<'a>),
+    UserNotice(UserNotice<'a>),
+    UserState(UserState<'a>),
+    Whisper(Whisper<'a>),
 }
 
 impl<'a> FromIrcMessage<'a> for AllCommands<'a> {
     type Error = InvalidMessage;
 
     fn from_irc(msg: IrcMessage<'a>) -> Result<Self, Self::Error> {
+        macro_rules! map {
+            ($ident:ident) => {
+                Self::$ident($ident::from_irc(msg)?)
+            };
+        }
+
+        use IrcMessage as M;
         let this = match msg.get_command() {
-            "001" => Self::IrcReady(IrcReady::from_irc(msg)?),
-            "376" => Self::Ready(Ready::from_irc(msg)?),
-            "CAP" => Self::Cap(Cap::from_irc(msg)?),
-            "CLEARCHAT" => Self::ClearChat(ClearChat::from_irc(msg)?),
-            // "CLEARMSG" => Self::ClearMsg(ClearMsg::from_irc(msg)?),
-            // "GLOBALUSERSTATE" => Self::GlobalUserState(GlobalUserState::from_irc(msg)?),
-            // "HOSTARGET" => Self::HostTarget(HostTarget::from_irc(msg)?),
-            // "JOIN" => Self::Join(Join::from_irc(msg)?),
-            // "NOTICE" => Self::Notice(Notice::from_irc(msg)?),
-            // "PART" => Self::Part(Part::from_irc(msg)?),
-            // "PING" => Self::Ping(Ping::from_irc(msg)?),
-            // "PONG" => Self::Pong(Pong::from_irc(msg)?),
-            // "PRIVMSG" => Self::Privmsg(Privmsg::from_irc(msg)?),
-            // "RECONNECT" => Self::Reconnect(Reconnect::from_irc(msg)?),
-            // "ROOMSTATE" => Self::RoomState(RoomState::from_irc(msg)?),
-            // "USERNOTICE" => Self::UserNotice(UserNotice::from_irc(msg)?),
-            // "USERSTATE" => Self::UserState(UserState::from_irc(msg)?),
-            // "WHISPER" => Self::Whisper(Whisper::from_irc(msg)?),
+            M::IRCREADY => map!(IrcReady),
+            M::READY => map!(Ready),
+            M::CAP => map!(Cap),
+            M::CLEARCHAT => map!(ClearChat),
+            M::CLEARMSG => map!(ClearMsg),
+            M::GLOBALUSERSTATE => map!(GlobalUserState),
+            M::HOSTTARGET => map!(HostTarget),
+            M::JOIN => map!(Join),
+            M::NOTICE => map!(Notice),
+            M::PART => map!(Part),
+            M::PING => map!(Ping),
+            M::PONG => map!(Pong),
+            M::PRIVMSG => map!(Privmsg),
+            M::RECONNECT => map!(Reconnect),
+            M::ROOMSTATE => map!(RoomState),
+            M::USERNOTICE => map!(UserNotice),
+            M::USERSTATE => map!(UserState),
+            M::WHISPER => map!(Whisper),
             _ => Self::Raw(IrcMessage::from_irc(msg).expect("infallible conversion")),
         };
 
         Ok(this)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ensure_const_match() {
+        let s = ":test!test@test PRIVMSG #museun :this is a test\r\n";
+        let msg = IrcMessage::parse(Str::Borrowed(s));
+        assert!(matches!(AllCommands::from_irc(msg).unwrap(), AllCommands::Privmsg{..}));
     }
 }
