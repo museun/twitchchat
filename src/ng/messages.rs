e7f07de53f5600070d763245dac05beba437d626
museun <museun@outlook.com>
2020-08-01 06:42:15 -0400

    move messages macros into messages

--- src/ng/messages.rs
+++ src/ng/messages.rs
@@ -1,5 +1,40 @@
 use super::{IrcMessage, Str, StrIndex};
 
+macro_rules! serde_struct {
+    (@one $($x:tt)*) => { () };
+    (@len $($e:expr),*) => { <[()]>::len(&[$(serde_struct!(@one $e)),*]); };
+
+    ($ty:ident { $($field:ident),* $(,)? }) => {
+        #[cfg(feature = "serde")]
+        impl<'t> ::serde::Serialize for $ty<'t> {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: ::serde::Serializer,
+            {
+                use ::serde::ser::SerializeStruct as _;
+                let len = serde_struct!(@len $($field),*);
+                let mut s = serializer.serialize_struct(stringify!($ty), len)?;
+                $( s.serialize_field(stringify!($field), &self.$field())?; )*
+                s.end()
+            }
+        }
+
+        serde_struct!{ @de $ty }
+    };
+
+    (@de $ty:ident) => {
+        #[cfg(feature = "serde")]
+        impl<'de, 't> ::serde::Deserialize<'de> for $ty<'t> {
+            fn deserialize<D>(deserializer: D) -> Result<$ty<'t>, D::Error>
+            where
+                D: ::serde::Deserializer<'de>,
+            {
+                deserializer.deserialize_map($crate::ng::serde::RawVisitor::default())
+            }
+        }
+    };
+}
+
 // TODO get rid of these macros
 macro_rules! raw {
     () => {
