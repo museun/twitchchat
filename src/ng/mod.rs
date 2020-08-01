bdd2d8d10e01a3f195b41700cd492ea19d7585cc
museun <museun@outlook.com>
2020-08-01 06:44:44 -0400

    general cleanups

--- src/ng/mod.rs
+++ src/ng/mod.rs
@@ -1,24 +1,14 @@
-// this has to be first for the macro
-#[cfg(feature = "serde")]
-mod serde;
-
 pub mod decoder;
 pub use decoder::{Decoder, DecoderAsync, Error as DecodeError};
 
 mod dispatcher;
 pub use dispatcher::{DispatchError, Dispatcher};
 
-// a public dep
-pub use simple_event_map::{EventMap, EventStream};
-
 mod encoder;
 pub use encoder::{AsyncEncoder, Encodable, Encoder};
 
 pub mod commands;
 pub mod messages;
-
-// do we want to expose this?
-mod channel;
 
 mod from_irc_message;
 pub use from_irc_message::{FromIrcMessage, InvalidMessage};
@@ -30,8 +20,13 @@ mod maybe_owned;
 pub use maybe_owned::{MaybeOwned as Str, MaybeOwnedIndex as StrIndex};
 
 pub mod validator;
-// TODO hide this ?
 use validator::Validator;
+
+// a public dep
+pub use simple_event_map::{EventMap, EventStream};
+
+#[cfg(feature = "serde")]
+mod serde;
 
 #[cfg(test)]
 mod commands_test;
