05268b105b802847b36d0569a5435e9a463afe76
museun <museun@outlook.com>
2020-08-06 02:01:33 -0400

    move trait into irc module

--- src/irc.rs
+++ src/irc.rs
@@ -1,5 +1,16 @@
-mod from_irc_message;
-pub use from_irc_message::FromIrcMessage;
+use std::convert::Infallible;
+
+pub trait FromIrcMessage<'a>: Sized {
+    type Error;
+    fn from_irc(msg: IrcMessage<'a>) -> Result<Self, Self::Error>;
+}
+
+impl<'a> FromIrcMessage<'a> for IrcMessage<'a> {
+    type Error = Infallible;
+    fn from_irc(msg: IrcMessage<'a>) -> Result<Self, Self::Error> {
+        Ok(msg)
+    }
+}
 
 mod message;
 pub use message::IrcMessage;
@@ -26,6 +37,6 @@ pub fn parse_one(input: &str) -> Result<(usize, IrcMessage<'_>), Error> {
     let pos = input
         .find("\r\n")
         .ok_or_else(|| Error::IncompleteMessage { pos: 0 })?;
-
-    Ok((pos, IrcMessage::parse(crate::Str::Borrowed(&input[..pos]))?))
+    let msg = IrcMessage::parse(crate::Str::Borrowed(&input[..pos]))?;
+    Ok((pos, msg))
 }
