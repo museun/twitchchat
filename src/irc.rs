58d109568e03b4322f7fddde77bbc54005f468ef
museun <museun@outlook.com>
2020-08-06 03:48:39 -0400

    add missing docs for the irc module

--- src/irc.rs
+++ src/irc.rs
@@ -1,6 +1,11 @@
+//! IRC message parsing.
+//!
+//! Twitch's chat is based on IRC, but not strictly conformant to the RFC-1459 and RFC-2812 specs.
+//!
+//! This provides a _Twitch-flavored_ IRC parser and types.
 use std::convert::Infallible;
 
-/// A trait to convert an IrcMessage into 'Self'.
+/// A trait to convert an `IrcMessage` into `Self`.
 pub trait FromIrcMessage<'a>: Sized {
     type Error;
     fn from_irc(msg: IrcMessage<'a>) -> Result<Self, Self::Error>;
@@ -29,11 +34,18 @@ mod error;
 pub use error::InvalidMessage;
 
 mod parser;
+pub use parser::IrcParserIter;
 
-pub fn parse(input: &str) -> impl Iterator<Item = Result<IrcMessage<'_>, InvalidMessage>> + '_ {
-    parser::IrcParserIter::new(input)
+/// Parses a string and iterators an iterator over the `IrcMessages` in it.
+///
+/// This borrows from the input string.
+pub fn parse(input: &str) -> IrcParserIter<'_> {
+    IrcParserIter::new(input)
 }
 
+/// Attempts to parse one message.
+///
+/// This returns the index of the /next/ message (e.g, 0 for a single message) and the parsed message
 pub fn parse_one(input: &str) -> Result<(usize, IrcMessage<'_>), InvalidMessage> {
     let pos = input
         .find("\r\n")
