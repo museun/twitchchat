b0bf91691eec2351fffcf56693c709d6ffca8fb4
museun <museun@outlook.com>
2020-08-05 20:35:49 -0400

    move to new prefix

--- src/ng/irc/prefix.rs
+++ src/ng/irc/prefix.rs
@@ -1,6 +1,7 @@
 use super::super::{Str, StrIndex};
 use crate::ng::IntoOwned;
 
+/// Prefix is the sender of a message
 pub struct Prefix<'a> {
     pub(crate) data: &'a Str<'a>,
     pub(crate) index: PrefixIndex,
@@ -13,38 +14,54 @@ impl<'a> std::fmt::Debug for Prefix<'a> {
 }
 
 impl<'a> Prefix<'a> {
+    /// Was this message from the server?
     pub fn is_server(&self) -> bool {
         !self.is_user()
     }
 
+    /// Was this message from a user?
     pub fn is_user(&self) -> bool {
         matches!(self.index, PrefixIndex::User{ .. })
     }
 
+    /// Get the full, raw string
     pub fn get_raw_prefix(&self) -> &'a str {
         &self.data[self.index.as_index()]
     }
 
+    // Get the nickname of this prefix, if it was sent by a user
     pub fn get_nick(&self) -> Option<&'a str> {
         self.index.nick_index().map(|index| &self.data[index])
     }
 }
 
+/// Prefix is the sender of a message
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
 pub enum PrefixIndex {
-    User { nick: StrIndex },
-    Server { host: StrIndex },
+    /// A user prefix
+    User {
+        /// Index of the nickname
+        nick: StrIndex,
+    },
+    /// A server prefix
+    Server {
+        /// Index of the hostname
+        host: StrIndex,
+    },
 }
 
 impl PrefixIndex {
+    /// Was this message from the server?
     pub fn is_server(&self) -> bool {
         !self.is_nick()
     }
 
+    /// Was this message from a user?
     pub fn is_nick(&self) -> bool {
         matches!(self, PrefixIndex::User{ .. })
     }
 
+    /// Get the index of the nickname
     pub fn nick_index(self) -> Option<StrIndex> {
         match self {
             PrefixIndex::User { nick } => Some(nick),
@@ -52,6 +69,15 @@ impl PrefixIndex {
         }
     }
 
+    /// Get the index of the hostname
+    pub fn host_index(self) -> Option<StrIndex> {
+        match self {
+            PrefixIndex::Server { host } => Some(host),
+            PrefixIndex::User { .. } => None,
+        }
+    }
+
+    /// Consumes this returning the index
     pub fn as_index(self) -> StrIndex {
         match self {
             PrefixIndex::User { nick } => nick,
