df9fe631972281324c3f41488bdec5f154f563aa
museun <museun@outlook.com>
2020-08-10 16:08:39 -0400

    get rid of ConnectorConfig

--- twitchchat/src/connector/tokio.rs
+++ twitchchat/src/connector/tokio.rs
@@ -1,15 +1,21 @@
-use crate::connector::ConnectorConfig;
 use crate::BoxedFuture;
 
 /// A `tokio` connector. This does not use TLS
 pub struct Connector {
-    config: ConnectorConfig,
+    addrs: Vec<std::net::SocketAddr>,
 }
 
 impl Connector {
-    /// Create a new connector with the provided configuration
-    pub const fn new(config: ConnectorConfig) -> Self {
-        Self { config }
+    /// Create a Connector that connects to Twitch
+    pub fn twitch() -> Self {
+        Self::custom(crate::TWITCH_IRC_ADDRESS).expect("twitch DNS resolution")
+    }
+
+    /// Create a connector with provided address(es)
+    pub fn custom<A: std::net::ToSocketAddrs>(addrs: A) -> std::io::Result<Self> {
+        addrs.to_socket_addrs().map(|addrs| Self {
+            addrs: addrs.collect(),
+        })
     }
 }
 
@@ -17,10 +23,10 @@ impl crate::connector::Connector for Connector {
     type Output = tokio_util::compat::Compat<tokio::net::TcpStream>;
 
     fn connect(&mut self) -> BoxedFuture<std::io::Result<Self::Output>> {
-        let config = self.config.clone();
+        let addrs = self.addrs.clone();
         let fut = async move {
             use tokio_util::compat::Tokio02AsyncReadCompatExt as _;
-            let stream = tokio::net::TcpStream::connect(&*config.addrs).await?;
+            let stream = tokio::net::TcpStream::connect(&*addrs).await?;
             Ok(stream.compat())
         };
         Box::pin(fut)
@@ -39,24 +45,30 @@ mod tls {
     /// To use this type, ensure you set up the 'TLS Domain' in the configuration.
     ///
     /// The crate provides the 'TLS domain' for Twitch in the root of this crate.
+    #[derive(Debug, Clone, PartialEq)]
     pub struct ConnectorTls {
-        config: ConnectorConfig,
+        addrs: Vec<std::net::SocketAddr>,
+        tls_domain: String,
     }
 
     impl ConnectorTls {
         /// Create a new `tokio` TLS connector.
-        ///
-        /// If the `TLS Domain` in the configuration is empty, this will return an error.
-        ///
-        /// If you're unsure of which TLS domain to use, use the one in the root of this crate.
-        pub fn new(config: ConnectorConfig) -> std::io::Result<Self> {
-            if config.tls_domain.is_empty() {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    "you must provide a TLS domain",
-                ));
-            }
-            Ok(Self { config })
+        pub fn twitch() -> Self {
+            Self::custom(crate::TWITCH_IRC_ADDRESS, crate::TWITCH_TLS_DOMAIN)
+                .expect("twitch DNS resolution")
+        }
+
+        /// Create a new `tokio` TLS connector.
+        pub fn custom<A, D>(addrs: A, domain: D) -> std::io::Result<Self>
+        where
+            A: std::net::ToSocketAddrs,
+            D: Into<String>,
+        {
+            let tls_domain = domain.into();
+            addrs.to_socket_addrs().map(|addrs| Self {
+                addrs: addrs.collect(),
+                tls_domain,
+            })
         }
     }
 
@@ -65,12 +77,11 @@ mod tls {
             tokio_util::compat::Compat<tokio_rustls::client::TlsStream<tokio::net::TcpStream>>;
 
         fn connect(&mut self) -> BoxedFuture<std::io::Result<Self::Output>> {
-            let config = self.config.clone();
+            let this = self.clone();
             let fut = async move {
                 use tokio_util::compat::Tokio02AsyncReadCompatExt as _;
-                let domain =
-                    tokio_rustls::webpki::DNSNameRef::try_from_ascii_str(&config.tls_domain)
-                        .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err))?;
+                let domain = tokio_rustls::webpki::DNSNameRef::try_from_ascii_str(&this.tls_domain)
+                    .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err))?;
 
                 let connector: tokio_rustls::TlsConnector = std::sync::Arc::new({
                     let mut c = tokio_rustls::rustls::ClientConfig::new();
@@ -80,7 +91,7 @@ mod tls {
                 })
                 .into();
 
-                let stream = tokio::net::TcpStream::connect(&*config.addrs).await?;
+                let stream = tokio::net::TcpStream::connect(&*this.addrs).await?;
                 let stream = connector.connect(domain, stream).await?;
 
                 Ok(stream.compat())
