2b7738cfe778bb82d2e366fcb01093a2c3880577
museun <museun@outlook.com>
2020-08-10 18:26:49 -0400

    expose the channels

--- twitchchat/src/channel.rs
+++ twitchchat/src/channel.rs
@@ -1,19 +1,27 @@
+//! Simple async/sync channels used in various parts of this crate.
 use std::{
     pin::Pin,
     task::{Context, Poll},
 };
 
+/// An error on send
 pub enum TrySendError<T> {
+    /// The receiver was closed
     Closed(T),
+    /// The receiver was full
     Full(T),
 }
 
+/// Async and sync mpsc Sender.
 #[derive(Clone)]
 pub struct Sender<T> {
     inner: async_channel::Sender<T>,
 }
 
 impl<T> Sender<T> {
+    /// Send this item asynchronously.
+    ///
+    /// On failure, return the sent item.
     pub async fn send(&self, item: T) -> Result<(), T>
     where
         T: Send,
@@ -21,6 +29,9 @@ impl<T> Sender<T> {
         self.inner.send(item).await.map_err(|e| e.into_inner())
     }
 
+    /// Send this item synchronously.
+    ///
+    /// On failure, returns why and the item.
     pub fn try_send(&self, item: T) -> Result<(), TrySendError<T>> {
         self.inner.try_send(item).map_err(|e| match e {
             async_channel::TrySendError::Full(t) => TrySendError::Full(t),
@@ -30,6 +41,7 @@ impl<T> Sender<T> {
 }
 
 pin_project_lite::pin_project! {
+    /// Async and sync mpsc Receiver.
     #[derive(Clone)]
     pub struct Receiver<T> {
         #[pin]
@@ -38,6 +50,9 @@ pin_project_lite::pin_project! {
 }
 
 impl<T> Receiver<T> {
+    /// Asynchronously receives an item
+    ///
+    /// If this returns None, the Sender was closed
     pub async fn recv(&self) -> Option<T>
     where
         T: Send,
@@ -45,6 +60,9 @@ impl<T> Receiver<T> {
         self.inner.recv().await.ok()
     }
 
+    /// Synchronously receives an item
+    ///
+    /// If this returns None, the Sender was closed
     pub fn try_recv(&self) -> Option<T> {
         self.inner.try_recv().ok()
     }
@@ -58,13 +76,14 @@ impl<T> futures_lite::Stream for Receiver<T> {
     }
 }
 
-pub(crate) fn bounded<T>(cap: usize) -> (Sender<T>, Receiver<T>) {
+/// Create a bounded channel
+pub fn bounded<T>(cap: usize) -> (Sender<T>, Receiver<T>) {
     let (tx, rx) = async_channel::bounded(cap);
     (Sender { inner: tx }, Receiver { inner: rx })
 }
 
-#[allow(dead_code)]
-pub(crate) fn unbounded<T>() -> (Sender<T>, Receiver<T>) {
+/// Create an unbounded channel
+pub fn unbounded<T>() -> (Sender<T>, Receiver<T>) {
     let (tx, rx) = async_channel::unbounded();
     (Sender { inner: tx }, Receiver { inner: rx })
 }
