69bd6cfad02a92e7e9d5c1ac71c2f9a20965a676
museun <museun@outlook.com>
2020-08-18 17:28:02 -0400

    add !quit to simple_bot. and some comments

--- examples/simple_bot.rs
+++ examples/simple_bot.rs
@@ -1,52 +1,67 @@
-#![cfg_attr(debug_assertions, allow(dead_code,))]
-// TODO correct this
 // note this uses `smol`. you can use `tokio` or `async_std` or `async_io` if you prefer.
-
 use anyhow::Context as _;
 use std::collections::HashMap;
 
+// extensions to the Privmsg type
 use twitchchat::PrivmsgExt as _;
 use twitchchat::{
-    commands::{privmsg, reply},
     messages::{AllCommands, Privmsg},
-    runner::{AsyncRunner, Status},
+    runner::{AsyncRunner, NotifyHandle, Status},
     writer::{AsyncWriter, MpscWriter},
     UserConfig,
 };
 
 fn main() -> anyhow::Result<()> {
+    // you'll need a user configuration
     let user_config = get_user_config()?;
+    // and some channels to join
     let channels = channels_to_join()?;
 
     let start = std::time::Instant::now();
 
     let mut bot = Bot::default()
-        .with_command("!hello", |msg: &Privmsg<'_>, resp: &mut Writer| {
-            let output = format!("hello {}!", msg.name());
-            msg.reply(resp, &output).unwrap();
+        .with_command("!hello", |args: Args| {
+            let output = format!("hello {}!", args.msg.name());
+            // We can 'reply' to this message using a writer + our output message
+            args.msg.reply(args.writer, &output).unwrap();
         })
-        .with_command("!uptime", move |msg: &Privmsg<'_>, resp: &mut Writer| {
+        .with_command("!uptime", move |args: Args| {
             let output = format!("its been running for {:.2?}", start.elapsed());
-            msg.say(resp, &output).unwrap();
+            // We can send a message back (without quoting the sender)  using a writer + our output message
+            args.msg.say(args.writer, &output).unwrap();
+        })
+        .with_command("!quit", move |args: Args| {
+            // because we're using sync stuff, turn async into sync with smol!
+            smol::block_on(async move {
+                // calling this will cause read_message() to eventually return Status::Quit
+                args.quit.notify().await
+            });
         });
 
+    // run the bot in the executor
     smol::run(async move { bot.run(&user_config, &channels).await })
 }
 
 // to make things easier to type
 type Writer = AsyncWriter<MpscWriter>;
 
+struct Args<'a, 'b: 'a> {
+    msg: &'a Privmsg<'b>,
+    writer: &'a mut Writer,
+    quit: NotifyHandle,
+}
+
 trait Command: Send + Sync {
-    fn handle(&mut self, msg: &Privmsg<'_>, response: &mut Writer);
+    fn handle(&mut self, args: Args<'_, '_>);
 }
 
 impl<F> Command for F
 where
-    F: Fn(&Privmsg<'_>, &mut Writer),
+    F: Fn(Args<'_, '_>),
     F: Send + Sync,
 {
-    fn handle(&mut self, msg: &Privmsg<'_>, response: &mut Writer) {
-        (self)(msg, response)
+    fn handle(&mut self, args: Args<'_, '_>) {
+        (self)(args)
     }
 }
 
@@ -83,19 +98,37 @@ impl Bot {
 
     // the main loop of the bot
     async fn main_loop(&mut self, runner: &mut AsyncRunner) -> anyhow::Result<()> {
+        // this is clonable, but we can just share it via &mut
+        // this is rate-limited writer
         let mut writer = runner.writer();
+        // this is clonable, but using it consumes it.
+        // this is used to 'quit' the main loop
+        let quit = runner.quit_handle();
+
         loop {
+            // this drives the internal state of the crate
             match runner.next_message().await? {
+                // if we get a Privmsg (you'll get an AllCommands enum for all messages received)
                 Status::Message(AllCommands::Privmsg(pm)) => {
+                    // see if its a command and do stuff with it
                     if let Some(cmd) = Self::parse_command(pm.data()) {
                         if let Some(command) = self.commands.get_mut(cmd) {
                             println!("dispatching to: {}", cmd.escape_debug());
-                            command.handle(&pm, &mut writer);
+
+                            let args = Args {
+                                msg: &pm,
+                                writer: &mut writer,
+                                quit: quit.clone(),
+                            };
+
+                            command.handle(args);
                         }
                     }
                 }
+                // stop if we're stopping
                 Status::Quit | Status::Eof => break,
-                Status::Message(msg) => continue,
+                // ignore the rest
+                Status::Message(..) => continue,
             }
         }
 
@@ -116,6 +149,7 @@ fn get_env_var(key: &str) -> anyhow::Result<String> {
     std::env::var(key).with_context(|| format!("please set `{}`", key))
 }
 
+// channels can be either in the form of '#museun' or 'museun'. the crate will internally add the missing #
 fn channels_to_join() -> anyhow::Result<Vec<String>> {
     let channels = get_env_var("TWITCH_CHANNEL")?
         .split(',')
