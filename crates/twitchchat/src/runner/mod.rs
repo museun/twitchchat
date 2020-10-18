3683a0d40ef43437b86e384658a28c08bb5c8193
museun <museun@outlook.com>
2020-10-18 08:32:25 -0400

    correct runner module documentation

--- crates/twitchchat/src/runner/mod.rs
+++ crates/twitchchat/src/runner/mod.rs
@@ -1,14 +1,16 @@
 //! A set of runners for managing a `event loop`
 //!
-//! To use the [AsyncRunner]:
-//! 1. choose [Connector](crate::connector::Connector) from [connectors](crate::connector).
-//! 1. create a [UserConfig](crate::UserConfig).
-//! 1. create and connect the [AsyncRunner] via its [AsyncRunner::connect()] method
+//! To use the [`AsyncRunner`]:
+//! 1. create a [`UserConfig`](crate::UserConfig).
+//! 1. create and connect the [`AsyncRunner`] via its [`AsyncRunner::connect()`] method
+//!     1. this takes a [`futures_io::AsyncRead`][async_read] + [`futures_io::AsyncWrite`][async_write]
 //! 1. now you're connected to Twitch, so next things you can do.
-//!     1. join a channel with: [AsyncRunner::join()],
-//!     1. write messages with the [AsyncWriter](crate::writer::AsyncWriter) provided by [AsyncRunner::writer()].
-//!     1. signal you want to quit with the [AsyncRunner::quit_handle()]
+//!     1. join a channel with: [`AsyncRunner::join()`],
+//!     1. write messages with the [`AsyncWriter`](crate::writer::AsyncWriter) provided by [`AsyncRunner::writer()`].
+//!     1. signal you want to quit with the [`AsyncRunner::quit_handle()`]
 //!
+//! [async_read]: https://docs.rs/futures-io/0.3.6/futures_io/trait.AsyncRead.html
+//! [async_write]: https://docs.rs/futures-io/0.3.6/futures_io/trait.AsyncWrite.html
 
 mod status;
 pub use status::{Status, StepResult};
